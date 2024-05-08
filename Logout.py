d6fab926ed3a353b53bae7166f4ca20f70fae8c9
ffserro <ffserro@hotmail.com>
2024-05-08 14:06:28 -0300

    .

--- Logout.py
+++ Logout.py
@@ -25,8 +25,8 @@ st.session_state['email'] = email
 password = login_form.text_input('Senha', type='password')
 if login_form.form_submit_button('Entrar'):
 	try:
-		user = auth.sign_in_with_email_and_password(email, password)
-		query = list(db.child('usuarios').order_by_child('email').equal_to(st.session_state['email']).get().val().values())[0]
+		user = app.sign_in_with_email_and_password(email, password)
+		query = list(app.child('usuarios').order_by_child('email').equal_to(st.session_state['email']).get().val().values())[0]
 		st.session_state['username'] = query['usuario']
 		st.session_state['origem'] = query['origem']
 		st.session_state['authentication_status'] = True
