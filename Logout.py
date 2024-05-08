f0261379012063beefdcdb7b2cf0cdc343499d07
ffserro <ffserro@hotmail.com>
2024-05-08 13:22:12 -0300

    .

--- Logout.py
+++ Logout.py
@@ -6,7 +6,7 @@ import pyrebase
 
 import firebase_admin
 from firebase_admin import credentials
-cred = credentials.Certificate(json.dump(st.secrets.credentials))
+cred = credentials.Certificate(json.loads(json.dump(st.secrets.credentials)))
 firebase_admin.initialize_app(cred)
 
 
