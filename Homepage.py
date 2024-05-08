ff358b51aa58ca1f0345315af77dc94c0e7f3912
ffserro <ffserro@hotmail.com>
2024-05-08 11:47:56 -0300

    .

--- Homepage.py
+++ Homepage.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.title('SIMAD')
+from io import BytesIO
+import base64
+
+file_ = open("logo.png", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+st.markdown(f"<img style='display: block; margin-left: auto; margin-right: auto; width:40%;' src='data:image/png;base64,{data_url}' alt='ComGptPatNavSSE' width='500'>", unsafe_allow_html=True)
+
+st.markdown("<h1 style='text-align: center;'>SIMAD</h1>", unsafe_allow_html=True)
